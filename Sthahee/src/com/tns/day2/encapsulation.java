aa3765ce3e938fe53702376d1ae836dd68a3f7c2
thaheer1268 <sthaheer974@gmail.com>
2024-08-27 12:24:27 +0530

    qwertyuiosdfghjk

---
+++ Sthahee/src/com/tns/day2/encapsulation.java
@@ -0,0 +1,5 @@
+package com.tns.day2;
+
+public class encapsulation {
+
+}
---
+++ Sthahee/src/com/tns/sf09/thaheer123.java
@@ -0,0 +1,13 @@
+package com.tns.sf09;
+
+public class thaheer123 {
+
+	public static void main(String[] args) {
+		System.out.println("hello thaheer");
+		double value1=120d/7d;
+		System.out.println(value1);		
+		long value6=120/120;
+		System.out.println(value6);
+	}
+
+}
